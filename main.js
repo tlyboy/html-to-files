89859d3d3ad4a117a91dc3ea32f94d7dc4d0feef
Guany <1084072690@qq.com>
2022-06-18 19:42:55 +0800

    update

--- main.js
+++ main.js
@@ -33,8 +33,8 @@ function resolveCSS(htmlStr) {
   const r1 = regStyle.exec(htmlStr)
   // 3.3 将提取出来的样式字符串，做进一步的处理
   const newCSS = r1[0].replace('<style>', '').replace('</style>', '')
-  // 3.4 将提取出来的 css 样式，写入到 index.css 文件中
-  fs.writeFile(new URL('./dist/index.css', import.meta.url), newCSS, err => {
+  // 3.4 将提取出来的 css 样式，写入到 style.css 文件中
+  fs.writeFile(new URL('./dist/style.css', import.meta.url), newCSS, err => {
     if (err) return console.log('写入 CSS 样式文件失败！' + err.message)
     console.log('写入 CSS 样式文件成功！')
   })
@@ -46,8 +46,8 @@ function resolveJS(htmlStr) {
   const r2 = regScript.exec(htmlStr)
   // 4.3 将提取出来的脚本字符串，做进一步的处理
   const newJS = r2[0].replace('<script>', '').replace('</script>', '')
-  // 4.4 将提取出来的 js 脚本，写入到 index.js 文件中
-  fs.writeFile(new URL('./dist/index.js', import.meta.url), newJS, err => {
+  // 4.4 将提取出来的 js 脚本，写入到 main.js 文件中
+  fs.writeFile(new URL('./dist/main.js', import.meta.url), newJS, err => {
     if (err) return console.log('写入 JS 脚本文件失败！' + err.message)
     console.log('写入 JS 脚本文件成功！')
   })
@@ -57,8 +57,8 @@ function resolveJS(htmlStr) {
 function resolveHTML(htmlStr) {
   // 5.2 使用字符串的 replace 方法，把内嵌的 <script> 和 </script> 标签，替换为外联的 <link> 和 <script> 标签
   const newHTML = htmlStr
-    .replace(regStyle, '<link rel="stylesheet" href="./index.css">')
-    .replace(regScript, '<script src="./index.js"></script>')
+    .replace(regStyle, '<link rel="stylesheet" href="./style.css">')
+    .replace(regScript, '<script src="./main.js"></script>')
   // 5.3 将替换完之后的 html 代码，写入到 index.html 文件中
   fs.writeFile(new URL('./dist/index.html', import.meta.url), newHTML, err => {
     if (err) return console.log('写入 HTML 页面文件失败！' + err.message)
